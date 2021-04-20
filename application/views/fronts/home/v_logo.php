2ec2139e1166762e2f8b715fac1d1f207b46dfff
sansD92 <adjiedwisandy@gmail.com>
2021-04-20 11:57:48 +0700

    up

--- application/views/fronts/home/v_logo.php
+++ application/views/fronts/home/v_logo.php
@@ -4,22 +4,22 @@
         	<div class="col-md-12 animation" data-animation="fadeInUp" data-animation-delay="0.2s">
             	<div class="cl_logo_slider owl-carousel owl-theme" data-margin="30" data-loop="true" data-autoplay="true" data-dots="false" data-autoplay-timeout="2000">
                 	<div class="item">
-                    	<a href="#"><img src="<?php echo base_url()?>assets/images/cl_logo1.png" alt="cl_logo1"/></a>
+                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/PASTE3-1536x1464.png" alt="cl_logo1"/></a>
                     </div>
                     <div class="item">
-                    	<a href="#"><img src="<?php echo base_url()?>assets/images/cl_logo2.png" alt="cl_logo2"/></a>
+                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/camn-1536x1464.png" alt="cam"/></a>
                     </div>
                     <div class="item">
-                    	<a href="#"><img src="<?php echo base_url()?>assets/images/cl_logo3.png" alt="cl_logo3"/></a>
+                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/wms3-1536x1464.png" alt="cam"/></a>
                     </div>
                     <div class="item">
-                    	<a href="#"><img src="<?php echo base_url()?>assets/images/cl_logo4.png" alt="cl_logo4"/></a>
+                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/wmu23-1536x1464.png" alt="cam"/></a>
                     </div>
                     <div class="item">
-                    	<a href="#"><img src="<?php echo base_url()?>assets/images/cl_logo5.png" alt="cl_logo5"/></a>
+                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/wci2-1536x1464.png" alt="cam"/></a>
                     </div>
                     <div class="item">
-                    	<a href="#"><img src="<?php echo base_url()?>assets/images/cl_logo6.png" alt="cl_logo6"/></a>
+                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/lmp23-1536x1464.png" alt="cam"/></a>
                     </div>
                 </div>
             </div>
