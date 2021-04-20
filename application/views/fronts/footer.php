b52fd169434530dec674b5724906856d3468591b
sansD92 <adjiedwisandy@gmail.com>
2021-04-20 18:47:16 +0700

    up

--- application/views/fronts/footer.php
+++ application/views/fronts/footer.php
@@ -36,84 +36,7 @@ Indonesia
                         </li>
                     </ul>
                 </div>
-<<<<<<< HEAD
                 
-=======
-                <div class="col-lg-2 col-md-6 mb-4 mb-lg-0 animation" data-animation="fadeInUp" data-animation-delay="0.4s">
-                    <h6 class="widget_title">USEFUL LINKS</h6>
-                    <ul class="list_none widget_links">
-                        <li><a href="#">Buy Anger Now</a></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Features</a></li>
-                        <li><a href="#">Feedback</a></li>
-                        <li><a href="#">Support center</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                    </ul>
-                </div>
-                <div class="col-lg-3 col-md-6 mb-4 mb-lg-0 animation" data-animation="fadeInUp" data-animation-delay="0.6s">
-                    <h6 class="widget_title">RECENT POSTS</h6>
-                    <ul class="recent_post border_bottom_dash list_none">
-                        <li>
-                            <div class="post_footer">
-                                <div class="post_img">
-                                    <a href="#"><img src="<?php echo base_url()?>assets/images/letest_post1.jpg" alt="letest_post1" /></a>
-                                </div>
-                                <div class="post_content">
-                                    <h6><a href="#">Lorem ipsum dolor sit amet, consectetur</a></h6>
-                                    <p class="small m-0">April 14, 2018</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="post_footer">
-                                <div class="post_img">
-                                    <a href="#"><img src="<?php echo base_url()?>assets/images/letest_post2.jpg" alt="letest_post2" /></a>
-                                </div>
-                                <div class="post_content">
-                                    <h6><a href="#">Lorem ipsum dolor sit amet, consectetur</a></h6>
-                                    <p class="small m-0">April 14, 2018</p>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div class="post_footer">
-                                <div class="post_img">
-                                    <a href="#"><img src="<?php echo base_url()?>assets/images/letest_post3.jpg" alt="letest_post3" /></a>
-                                </div>
-                                <div class="post_content">
-                                    <h6><a href="#">Lorem ipsum dolor sit amet, consectetur</a></h6>
-                                    <p class="small m-0">April 14, 2018</p>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-lg-4 col-md-6 animation" data-animation="fadeInUp" data-animation-delay="0.8s">
-                    <h6 class="widget_title">Get in Touch</h6>
-                    <div class="field_form widget_contact">
-                        <form method="post" name="enq">
-                        <div class="row">
-                            <div class="form-group col-md-12">
-                                <input type="text" required="required" placeholder="Enter Name *" id="first-name" class="form-control" name="name">
-                             </div>
-                            <div class="form-group col-md-12">
-                                <input type="email" required="required" placeholder="Enter Email *" id="email" class="form-control" name="email">
-                            </div>
-
-                            <div class="form-group col-md-12">
-                                <textarea required="required" placeholder="Message *" id="description" class="form-control" name="message" rows="3"></textarea>
-                            </div>
-                            <div class="col-md-12">
-                                <button type="submit" title="Submit Your Message!" class="btn btn-default" id="submitButton" name="submit" value="Submit">Submit</button>
-                            </div>
-                            <div class="col-md-12">
-                                <div id="alert-msg" class="alert-msg text-center"></div>
-                            </div>
-                        </div>
-                    </form>
-                    </div>
-                </div>
->>>>>>> 29df83c2c684e94f5ea5f7fa6e95b7cc42ca8334
             </div>
         </div>
     </div>
@@ -125,7 +48,6 @@ Indonesia
                     <p class="copyright m-md-0 text-center text-md-left">&copy; WMP 2021</p>
                 </div>
                 <div class="col-md-6">
-<<<<<<< HEAD
                  
                 </div>
                 <div class="col-md-3 ">
@@ -143,17 +65,11 @@ Indonesia
                     <br>
                     <br>
                      <ul class="list_none widget_links_style1">
+                        <li><a href="#">Kontak Kami</a></li>
+                        <li><a href="#">Catatan Legal</a></li>
                          <li><a href="#">Kontak Kami</a></li>
                         <li><a href="#">Catatan Legal</a></li>
-                         <li><a href="#">Sitemap</a></li>
-                        <li><a href="#">Akses</a></li>
                         
-=======
-                    <ul class="list_none footer_social text-center text-md-right">
-                        <li><a href="#"><i class="ion-social-youtube-outline"></i></a></li>
-                        <li><a href="#"><i class="ion-social-facebook"></i></a></li>                        
-                        <li><a href="#"><i class="ion-social-instagram-outline"></i></a></li>
->>>>>>> 29df83c2c684e94f5ea5f7fa6e95b7cc42ca8334
                     </ul>
                 </div>
 
