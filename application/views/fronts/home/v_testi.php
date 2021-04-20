ba643d4dc70b405d9dc1b702fc120b027112606b
arkans <82564511+arkansys22@users.noreply.github.com>
2021-04-20 13:34:36 +0700

    up

--- application/views/fronts/home/v_testi.php
+++ application/views/fronts/home/v_testi.php
@@ -1,10 +1,10 @@
-<section>	
+<section>
 	<div class="container">
     	<div class="row">
         	<div class="col-md-12 animation" data-animation="fadeInUp" data-animation-delay="0.2s">
             	<div class="heading_s4 text-center">
-                	<span class="sub_title">Clients Says</span>
-                	<h2>Testimonials</h2>
+                	<span class="sub_title">Target Perusahaan</span>
+                	<h2>Arah Pengembangan WMP</h2>
                 </div>
             </div>
         </div>
@@ -17,48 +17,36 @@
                             	<img class="rounded-circle m-auto" src="<?php echo base_url()?>assets/images/user1.jpg" alt="user"/>
                             </div>
                             <div class="testi_meta">
-                                <h6>Lissa Castro</h6>
-                                <span>Co-Founder</span>
-                            	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, quaeillo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                                <h6>Nama Lengkap</h6>
+                                <span>Owner</span>
+                            	<p>kalimat arahan / quotes</p>
                             </div>
                         </div>
                     </div>
-                    <div class="item">
-                    	<div class="testimonial_box">
-                        	<div class="testimonial_img">
-                            	<img class="rounded-circle m-auto" src="<?php echo base_url()?>assets/images/user2.jpg" alt="user"/>
-                            </div>
-                            <div class="testi_meta">
-                                <h6>Alden Smith</h6>
-                                <span>Creative Designer</span>
-                            	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, quaeillo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                    	<div class="testimonial_box">
-                        	<div class="testimonial_img">
-                            	<img class="rounded-circle m-auto" src="<?php echo base_url()?>assets/images/user3.jpg" alt="user"/>
-                            </div>
-                            <div class="testi_meta">
-                                <h6>Daisy Lana</h6>
-                                <span>Creative Director</span>
-                            	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, quaeillo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item">
-                    	<div class="testimonial_box">
-                        	<div class="testimonial_img">
-                            	<img class="rounded-circle m-auto" src="<?php echo base_url()?>assets/images/user4.jpg" alt="user"/>
-                            </div>
-                            <div class="testi_meta">
-                                <h6>Alfred Amos</h6>
-                                <span>Creative Designer</span>
-                            	<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, quaeillo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                            </div>
-                        </div>
-                    </div>
+										<div class="item">
+	                    	<div class="testimonial_box">
+	                        	<div class="testimonial_img">
+	                            	<img class="rounded-circle m-auto" src="<?php echo base_url()?>assets/images/user1.jpg" alt="user"/>
+	                            </div>
+	                            <div class="testi_meta">
+	                                <h6>Nama Lengkap</h6>
+	                                <span>Direktur</span>
+	                            	<p>kalimat arahan / quotes</p>
+	                            </div>
+	                        </div>
+	                    </div>
+											<div class="item">
+		                    	<div class="testimonial_box">
+		                        	<div class="testimonial_img">
+		                            	<img class="rounded-circle m-auto" src="<?php echo base_url()?>assets/images/user1.jpg" alt="user"/>
+		                            </div>
+		                            <div class="testi_meta">
+		                                <h6>Nama Lengkap</h6>
+		                                <span>Direktur</span>
+		                            	<p>kalimat arahan / quotes</p>
+		                            </div>
+		                        </div>
+		                    </div>
                 </div>
             </div>
         </div>
