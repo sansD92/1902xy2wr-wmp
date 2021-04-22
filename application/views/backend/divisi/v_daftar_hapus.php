9c5c149921c163000f31e62c1cbea65d4ab1f340
arkans <82564511+arkansys22@users.noreply.github.com>
2021-04-22 11:49:51 +0700

    up

--- application/views/backend/divisi/v_daftar_hapus.php
+++ application/views/backend/divisi/v_daftar_hapus.php
@@ -6,12 +6,13 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1>Daftar Artikel</h1>
+          <h1>Divisi Terhapus</h1>
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="home">Dashboard</a></li>
-            <li class="breadcrumb-item active">Daftar Artikel</li>
+            <li class="breadcrumb-item"><a href="<?php echo base_url()?>">Home</a></li>
+            <li class="breadcrumb-item active"><a href="<?php echo base_url()?>aspanel/divisi">Daftar Divisi</a></li>
+            <li class="breadcrumb-item active">Divisi Terhapus</li>
           </ol>
         </div>
       </div>
@@ -23,32 +24,31 @@
         <div class="col-12">
           <div class="card">
             <div class="card-header">
-              <h3 class="card-title"><a class="btn btn-success btn-sm" title="Edit Data" href="<?php echo base_url()?>aspanel/blogs_tambahkan"><i class="fas fa-plus-circle"></i> Tambahkan</a></h3>
-              <h3 class="text-right"><a class="btn btn-danger btn-sm" title="Edit Data" href="<?php echo base_url()?>aspanel/blogs_storage_bin"><i class="fas fa-trash"></i> Sampah</a></h3>
+              <h3 class="card-title"><a class="btn btn-success btn-sm" title="Edit Data" href="<?php echo base_url()?>aspanel/divisi">Kembali Ke Daftar Divisi</a></h3>
             </div>
             <div class="card-body">
               <table id="example1" class="table table-bordered table-striped">
                 <thead>
                 <tr>
-                  <th>Tanggal</th>
-                  <th>Judul</th>
-                  <th>Aksi</th>
+                  <th>Dibuat</th>
+                  <th>Kode</th>
+                  <th>Action</th>
                 </tr>
                 </thead>
                 <tbody>
                 <?php
                 $no = 1;
                 foreach ($record as $row){
-                  $tgl_posting = $this->mylibrary->tgl_indo($row['blogs_post_tanggal']);
+                  $tgl_posting = $this->mylibrary->tgl_indo($row['divisi_post_tanggal']);
                   ?>
 
                 <tr>
                   <td><?=$tgl_posting?></td>
-                  <td><a class="btn btn-primary btn-sm" title="Lihat" href="<?php echo base_url()?>blogs/<?=$row['blogs_judul_seo']?>" target="_blank"> <i class="fas fa-eye"> <?=$row['blogs_dibaca']?></i></a><?=$row['blogs_judul']?></td>
+                  <td><?=$row['divisi_meta_desk']?></td>
                   <td>
                     <?php
-                    echo"<a class='btn btn-primary btn-sm' title='Edit Data' href='".base_url()."aspanel/blogs_update/$row[blogs_id]'><i class='fas fa-edit'></i></a>
-                    <a class='btn btn-danger btn-sm' title='Delete Data' href='".base_url()."aspanel/blogs_delete_temp/$row[blogs_id]' onclick=\"return confirm('Are you sure want to delete this data?')\"><i class='fas fa-trash-alt'></i></a>";
+                    echo"<a class='btn btn-primary btn-sm' title='Kembalikan' href='".base_url()."aspanel/divisi_restore/$row[divisi_id]' onclick=\"return confirm('Yakin ingin mengembalikan data ini?')\"><i class='fab fa-creative-commons-sa'></i></a>
+                    <a class='btn btn-danger btn-sm' title='Permanently Delete' href='".base_url()."aspanel/divisi_delete/$row[divisi_id]' onclick=\"return confirm('Yakin untuk menghapus data ini secara permanen?')\"><i class='fas fa-trash-alt'></i></a>";
                     ?>
                   </td>
                 </tr>
@@ -57,9 +57,9 @@
                 </tbody>
                 <tfoot>
                 <tr>
-                 <th>Tanggal</th>
-                  <th>Judul</th>
-                  <th>Aksi</th>
+                  <th>Date</th>
+                  <th>Title</th>
+                  <th>Action</th>
                 </tr>
                 </tfoot>
               </table>
