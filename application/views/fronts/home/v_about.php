e6f70c406703a49dd0993836a4725aa1f0f1d4c8
sansD92 <adjiedwisandy@gmail.com>
2021-04-23 08:08:18 +0700

    up

--- application/views/fronts/home/v_about.php
+++ application/views/fronts/home/v_about.php
@@ -4,7 +4,7 @@
             <div class="col-md-6" >
                 <div class="overlay_bg about_video_img mb-4 mb-lg-0">
                     <img src="<?php echo base_url()?>assets/frontend/macam.jpg" alt="about_img4"/>
-                    <a href="https://www.youtube.com/watch?v=BxWT2FV8kTs" class="video_popup video_play"><img class="bounce_img" src="<?php echo base_url()?>assets/images/play-icon.png" alt="play-icon"></a>
+                    <a href="https://www.youtube.com/watch?v=jPSzKyY5NnY" class="video_popup video_play"><img class="bounce_img" src="<?php echo base_url()?>assets/images/play-icon.png" alt="play-icon"></a>
                 </div>
             </div>
             <div class="col-md-6 col-sm-12 animation" data-animation="fadeInRight" data-animation-delay="0.4s">
