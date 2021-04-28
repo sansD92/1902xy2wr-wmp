771169876b249b41f57fa7671ce2b00e2e00135f
sansD92 <adjiedwisandy@gmail.com>
2021-04-28 14:56:18 +0700

    up

---
+++ application/views/fronts/Berita/v_berita.php
@@ -0,0 +1,305 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- START SECTION HEAD -->
+<?php $this->load->view('fronts/head.php')?>
+<!-- END SECTION HEAD -->
+
+<body>
+
+
+<!-- START HEADER -->
+<?php $this->load->view('fronts/header.php')?>
+<!-- END HEADER -->
+
+<!-- START SECTION BANNER -->
+<section class="background_bg breadcrumb_section overlay_bg2 page-title-light" data-img-src="assets/images/about_bg.jpg">
+	<div class="container">
+    	<div class="row">
+        	<div class="col-md-12 text-center">
+            	<div class="page-title">
+            		<h1></h1>
+                </div>
+                <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb justify-content-center">
+                    <li class="breadcrumb-item"><a href="#">Beranda</a></li>
+                    <li class="breadcrumb-item active" aria-current="page"></li>
+                  </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END SECTION BANNER -->
+
+<!-- START SECTION PORTFOLIO SLIDER -->
+<section>
+  <div class="container">
+      <div class="row">
+          <div class="col-lg-9">
+              <div class="row">
+                    <div class="col-lg-4 col-md-6 mb-md-4 mb-2 pb-2">
+                        <div class="blog_post blog_style1">
+                            <div class="blog_img">
+                                <a href="#">
+                                    <img src="assets/images/blog_small_img1_350X198.jpg" alt="blog_small_img1_350X198">
+                                </a>
+                            </div>
+                            <div class="blog_content bg-white">
+                                <div class="blog_text">
+                                    <h6 class="blog_title"><a href="#">Why are tickets to fly to Lagos expensive?</a></h6>
+                                    <ul class="list_none blog_meta">
+                                        <li><a href="#"><i class="ion-calendar"></i> April 14, 2018</a></li>
+                                        <li><a href="#"><i class="ion-chatboxes"></i> 2 Comment</a></li>
+                                    </ul>
+                                    <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text</p>
+                                    <a href="#" class="text-capitalize">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-md-4 mb-2 pb-2">
+                        <div class="blog_post blog_style1">
+                            <div class="blog_img">
+                                <a href="#">
+                                    <img src="assets/images/blog_small_img2_350X198.jpg" alt="blog_small_img2_350X198">
+                                </a>
+                            </div>
+                            <div class="blog_content bg-white">
+                                <div class="blog_text">
+                                    <h6 class="blog_title"><a href="#">A cheap and flexible solution for those who want a starter package </a></h6>
+                                    <ul class="list_none blog_meta">
+                                        <li><a href="#"><i class="ion-calendar"></i> April 14, 2018</a></li>
+                                        <li><a href="#"><i class="ion-chatboxes"></i> 2 Comment</a></li>
+                                    </ul>
+                                    <p>Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+                                    <a href="#" class="text-capitalize">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-md-4 mb-2 pb-2">
+                        <div class="blog_post blog_style1">
+                            <div class="blog_img">
+                                <a href="#">
+                                    <img src="assets/images/blog_small_img3_350X198.jpg" alt="blog_small_img3_350X198">
+                                </a>
+                            </div>
+                            <div class="blog_content bg-white">
+                                <div class="blog_text">
+                                    <h6 class="blog_title"><a href="#">But I must explain to you how all this mistaken idea of denouncing</a></h6>
+                                    <ul class="list_none blog_meta">
+                                        <li><a href="#"><i class="ion-calendar"></i> April 14, 2018</a></li>
+                                        <li><a href="#"><i class="ion-chatboxes"></i> 2 Comment</a></li>
+                                    </ul>
+                                    <p>It uses a dictionary of over combined with a handful of model sentence structures, to generate Lorem Ipsum which looks</p>
+                                    <a href="#" class="text-capitalize">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-md-4 mb-2 pb-2">
+                        <div class="blog_post blog_style1">
+                            <div class="blog_img">
+                                <div class="fit-videos">
+                                    <iframe src="https://player.vimeo.com/video/231236542?title=0&byline=0&portrait=0" width="540" height="304" allowfullscreen></iframe>
+                                </div>
+                            </div>
+                            <div class="blog_content bg-white">
+                                <div class="blog_text">
+                                    <h6 class="blog_title"><a href="#">The Problem With Typefaces on the Web</a></h6>
+                                    <ul class="list_none blog_meta">
+                                        <li><a href="#"><i class="ion-calendar"></i> April 14, 2018</a></li>
+                                        <li><a href="#"><i class="ion-chatboxes"></i> 2 Comment</a></li>
+                                    </ul>
+                                    <p>It uses a dictionary of over combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable</p>
+                                    <a href="#" class="text-capitalize">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-md-4 mb-2 pb-2">
+                        <div class="blog_post blog_style1">
+                            <div class="blog_img">
+                                <iframe width="100%" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/118951274&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+                            </div>
+                            <div class="blog_content bg-white">
+                                <div class="blog_text">
+                                    <h6 class="blog_title"><a href="#">How to Choose the Best Font for Your Blog's Body Copy</a></h6>
+                                    <ul class="list_none blog_meta">
+                                        <li><a href="#"><i class="ion-calendar"></i> April 14, 2018</a></li>
+                                        <li><a href="#"><i class="ion-chatboxes"></i> 2 Comment</a></li>
+                                    </ul>
+                                    <p>It uses a dictionary of over combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable</p>
+                                    <a href="#" class="text-capitalize">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-md-4 mb-2 pb-2">
+                        <div class="blog_post blog_style1">
+                            <div class="blog_img">
+                                <div class="carousel_slide1 owl-carousel owl-theme dots_white" data-autoheight="true" data-autoplay="true" data-loop="true" data-animate-in="fadeIn" data-animate-out="fadeOut" data-autoplay-timeout="3000">
+                                    <a href="#">
+                                        <img src="assets/images/blog_small_img4_350X198.jpg" alt="blog_small_img4_350X198">
+                                    </a>
+                                    <a href="#">
+                                    <img src="assets/images/blog_small_img5_350X198.jpg" alt="blog_small_img5_350X198">
+                                </a>
+                                </div>
+                            </div>
+                            <div class="blog_content bg-white">
+                                <div class="blog_text">
+                                    <h6 class="blog_title"><a href="#">dignissimos ducimus qui blanditiis praesentium voluptatum deleniti</a></h6>
+                                    <ul class="list_none blog_meta">
+                                        <li><a href="#"><i class="ion-calendar"></i> April 14, 2018</a></li>
+                                        <li><a href="#"><i class="ion-chatboxes"></i> 2 Comment</a></li>
+                                    </ul>
+                                    <p>It uses a dictionary of over combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable</p>
+                                    <a href="#" class="text-capitalize">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-md-4 mb-2 pb-2">
+                        <div class="blog_post blog_style1">
+                            <div class="blog_img">
+                                <a href="#">
+                                    <img src="assets/images/blog_small_img6_350X198.jpg" alt="blog_small_img6_350X198">
+                                </a>
+                            </div>
+                            <div class="blog_content bg-white">
+                                <div class="blog_text">
+                                    <h6 class="blog_title"><a href="#">who are so beguiled and demoralized by the charms of pleasure of the moment.</a></h6>
+                                    <ul class="list_none blog_meta">
+                                        <li><a href="#"><i class="ion-calendar"></i> April 14, 2018</a></li>
+                                        <li><a href="#"><i class="ion-chatboxes"></i> 2 Comment</a></li>
+                                    </ul>
+                                    <p>It uses a dictionary of over combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable</p>
+                                    <a href="#" class="text-capitalize">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-md-4 mb-2 pb-2">
+                        <div class="blog_post blog_style1">
+                            <div class="blog_img">
+                                <div class="fit-videos">
+                                    <iframe width="540" height="304" src="https://www.youtube.com/embed/7e90gBu4pas" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+                                </div>
+                            </div>
+                            <div class="blog_content bg-white">
+                                <div class="blog_text">
+                                    <h6 class="blog_title"><a href="#">which is the same as saying through shrinking from toil and pain</a></h6>
+                                    <ul class="list_none blog_meta">
+                                        <li><a href="#"><i class="ion-calendar"></i> April 14, 2018</a></li>
+                                        <li><a href="#"><i class="ion-chatboxes"></i> 2 Comment</a></li>
+                                    </ul>
+                                    <p>It uses a dictionary of over combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable</p>
+                                    <a href="#" class="text-capitalize">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 mt-4">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1"><i class="ion-ios-arrow-thin-left"></i></a></li>
+                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#"><i class="ion-ios-arrow-thin-right"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 mt-lg-0 mt-4 pt-3 pt-lg-0">
+              <div class="sidebar">
+                    <div class="widget">
+                            <div class="newsletter_form">
+                            <form> 
+                                <div class="outline_input-black">
+                                    <input required="" placeholder="Search..." type="text">
+                                </div>
+                                <button type="submit" title="Subscribe" class="btn btn-submit border-0" name="submit" value="Submit">
+                                    <span class="ti-search"></span>
+                                </button>
+                            </form>
+                        </div>
+                        </div>
+                
+                    <div class="widget">
+                      <h5 class="widget_title">MEDIA</h5>
+                        <ul class="list_none widget_categories border_bottom_dash">
+                            <li><a href="#"><span class="categories_name">Berita</span></a></li>
+                            <li><a href="#"><span class="categories_name">WMP Megazine</span></a></li>
+                            <li><a href="#"><span class="categories_name">Youtube WMP</span></a></li>
+                            
+                      </ul>
+                    </div>
+                  
+                    <div class="widget">
+                      <h5 class="widget_title">Produk</h5>
+                        <ul class="list_none widget_categories border_bottom_dash">
+                            <li><a href="#"><span class="categories_name">Sapi</span></a></li>
+                            <li><a href="#"><span class="categories_name">Daging Sapi</span></a></li>
+                            <li><a href="#"><span class="categories_name">Produk Olahan</span></a></li>
+                             <li><a href="#"><span class="categories_name">Kulit</span></a></li>
+                              <li><a href="#"><span class="categories_name">Produk Ayam</span></a></li>
+                               <li><a href="#"><span class="categories_name">Komoditas</span></a></li>
+                            
+                      </ul>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</section>
+<!-- END SECTION PORTFOLIO -->
+
+<!-- START SECTION SUBCRIBE -->
+<?php $this->load->view('fronts/home/v_subcribe')?>
+<!-- END SECTION SUBCRIBE -->
+
+<!-- START FOOTER SECTION -->
+<?php $this->load->view('fronts/footer')?>
+<!-- END FOOTER SECTION -->
+
+<a href="#" class="scrollup" style="display: none;"><i class="ion-ios-arrow-up"></i></a>
+
+<!-- Latest jQuery -->
+<script src="<?php echo base_url()?>assets/js/jquery-1.12.4.min.js"></script>
+<!-- jquery-ui -->
+<script src="<?php echo base_url()?>assets/js/jquery-ui.js"></script>
+<!-- popper min js -->
+<script src="<?php echo base_url()?>assets/js/popper.min.js"></script>
+<!-- Latest compiled and minified Bootstrap -->
+<script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap.min.js"></script>
+<!-- owl-carousel min js  -->
+<script src="<?php echo base_url()?>assets/owlcarousel/js/owl.carousel.min.js"></script>
+<!-- magnific-popup min js  -->
+<script src="<?php echo base_url()?>assets/js/magnific-popup.min.js"></script>
+<!-- waypoints min js  -->
+<script src="<?php echo base_url()?>assets/js/waypoints.min.js"></script>
+<!-- parallax js  -->
+<script src="<?php echo base_url()?>assets/js/parallax.js"></script>
+<!-- countdown js  -->
+<script src="<?php echo base_url()?>assets/js/jquery.countdown.min.js"></script>
+<!-- fit video  -->
+<script src="<?php echo base_url()?>assets/js/jquery.fitvids.js"></script>
+<!-- jquery.counterup.min js -->
+<script src="<?php echo base_url()?>assets/js/jquery.counterup.min.js"></script>
+<!-- isotope min js -->
+<script src="<?php echo base_url()?>assets/js/isotope.min.js"></script>
+<!-- isotope-loadmore js -->
+<script src='<?php echo base_url()?>assets/js/isotope-loadmore.js'></script>
+<!-- elevatezoom js -->
+<script src='<?php echo base_url()?>assets/js/jquery.elevatezoom.js'></script>
+<!-- scripts js -->
+<script src="<?php echo base_url()?>assets/js/scripts.js"></script>
+
+</body>
+</html>
