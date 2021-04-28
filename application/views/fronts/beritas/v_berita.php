7da45a6cd9e468d30027b7af8c43e5728230fad1
sansD92 <adjiedwisandy@gmail.com>
2021-04-28 16:49:22 +0700

    UP

--- application/views/fronts/beritas/v_berita.php
+++ application/views/fronts/beritas/v_berita.php
@@ -69,11 +69,10 @@
                 <div class="row">
                     <div class="col-12 mt-4">
                         <ul class="pagination justify-content-center">
-                            <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1"><i class="ion-ios-arrow-thin-left"></i></a></li>
-                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item"><a class="page-link" href="#"><i class="ion-ios-arrow-thin-right"></i></a></li>
+                            <li ><?php
+  echo $this->pagination->create_links();
+  ?></li>
+                            
                         </ul>
                     </div>
                 </div>
