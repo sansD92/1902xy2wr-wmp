14d57cf2758b2ee7663a07d3c75a04ce9dd58b86
sansD92 <adjiedwisandy@gmail.com>
2021-04-28 12:48:42 +0700

    UP

--- application/views/fronts/home/v_blog.php
+++ application/views/fronts/home/v_blog.php
@@ -16,11 +16,12 @@
         <div class="row animation" data-animation="fadeInUp" data-animation-delay="0.4s">
         	<div class="col-md-12">
             	<div class="blog_wrap carousel_slide3 owl-carousel owl-theme" data-margin="30" data-loop="true" data-autoplay="true" data-autoplay-timeout="2000">
-									<?php  foreach ($posts as $post_new){
-										$isi = character_limiter($post_new->blogs_desk,230);
-										$judul = character_limiter($post_new->blogs_judul,60);
-										?>
+									
 										<div class="items">
+                                            <?php  foreach ($posts as $post_new){
+                                        $isi = character_limiter($post_new->blogs_desk,230);
+                                        $judul = character_limiter($post_new->blogs_judul,60);
+                                        ?>
                         <div class="blog_post blog_style1">
                             <div class="blog_img">
                                 <a href="#">
@@ -35,11 +36,11 @@
                             </div>
                             <div class="blog_content bg-white">
                                 <div class="blog_text">
-                                    <h6 class="blog_title"><a href="<?php echo base_url("berita/$post_new->blogs_judul_seo") ?>"><?php echo $judul?></a></h6>
+                                    <h6 class="blog_title"><a href="<?php echo base_url("$post_new->blogs_judul_seo") ?>"><?php echo $judul?></a></h6>
                                     <ul class="list_none blog_meta">
                                         <li><a href="#"><i class="ion-calendar"></i><?php echo tgl_indo($post_new->blogs_post_tanggal)?></a></li>
                                     </ul>
-                                    <p><?php echo $isi?>.<a href="<?php echo base_url("berita/$post_new->blogs_judul_seo") ?>" class="text-capitalize">Baca Lebih Lengkap</a></p>
+                                    <p><?php echo $isi?>.<a href="<?php echo base_url("$post_new->blogs_judul_seo") ?>" class="text-capitalize">Baca Lebih Lengkap</a></p>
 
                                 </div>
                             </div>
