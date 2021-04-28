4f6f85b15438b6afe697f206c95fea7368909fa9
sansD92 <adjiedwisandy@gmail.com>
2021-04-28 13:04:46 +0700

    up

--- application/views/fronts/home/v_blog.php
+++ application/views/fronts/home/v_blog.php
@@ -1,52 +1,52 @@
-<section class="gray_bg" >
-	<div class="container">
-    	<div class="row">
-        	<div class="col-md-12">
-            	<div class="heading_s4 text-center">
-                	<span class="sub_title">Berita Terbaru</span>
-                	<h2>Berita WMP</h2>
+<section class="gray_bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 animation" data-animation="fadeInUp" data-animation-delay="0.2s">
+                <div class="heading_s4 text-center">
+                    <span class="sub_title">Letest News</span>
+                    <h2>Our Blog</h2>
                 </div>
             </div>
         </div>
         <div class="row">
-        	<div class="col-12">
-            	<div class="clearfix small_divider"></div>
+            <div class="col-12">
+                <div class="clearfix small_divider"></div>
             </div>
         </div>
         <div class="row animation" data-animation="fadeInUp" data-animation-delay="0.4s">
-        	<div class="col-md-12">
-            	<div class="blog_wrap carousel_slide3 owl-carousel owl-theme" data-margin="30" data-loop="true" data-autoplay="true" data-autoplay-timeout="2000">
-									<?php  foreach ($posts as $post_new){
-										$isi = character_limiter($post_new->blogs_desk,230);
-										$judul = character_limiter($post_new->blogs_judul,60);
-										?>
-										<div class="items">
+            <div class="col-md-12">
+                <div class="blog_wrap carousel_slide3 owl-carousel owl-theme" data-margin="30" data-loop="true" data-autoplay="true" data-autoplay-timeout="2000">
+                    <?php  foreach ($posts as $post_new){
+                                        $isi = character_limiter($post_new->blogs_desk,230);
+                                        $judul = character_limiter($post_new->blogs_judul,60);
+                                        ?>
+                    <div class="items">
                         <div class="blog_post blog_style1">
                             <div class="blog_img">
                                 <a href="#">
-																	<?php
-								                             if(empty($post_new->blogs_gambar)) {
-								                               echo "<img src='".base_url()."assets/images/blog_small_img1_350X198.jpg'>";
-								                             }else {
-								                               echo " <img src='".base_url()."assets/frontend/blogs/".$post_new->blogs_gambar."'> ";}
-								                             ?>
-
+                                   <?php
+                                                             if(empty($post_new->blogs_gambar)) {
+                                                               echo "<img src='".base_url()."assets/images/blog_small_img1_350X198.jpg'>";
+                                                             }else {
+                                                               echo " <img src='".base_url()."assets/frontend/blogs/".$post_new->blogs_gambar."'> ";}
+                                                             ?>
                                 </a>
                             </div>
                             <div class="blog_content bg-white">
                                 <div class="blog_text">
-                                    <h6 class="blog_title"><a href="<?php echo base_url("$post_new->blogs_judul_seo") ?>"><?php echo $judul?></a></h6>
+                                    <h6 class="<?php echo base_url("$post_new->blogs_judul_seo") ?>"><a href="#"><?php echo $judul?></a></h6>
                                     <ul class="list_none blog_meta">
                                         <li><a href="#"><i class="ion-calendar"></i><?php echo tgl_indo($post_new->blogs_post_tanggal)?></a></li>
                                     </ul>
-                                    <p><?php echo $isi?>.<a href="<?php echo base_url("$post_new->blogs_judul_seo") ?>" class="text-capitalize">Baca Lebih Lengkap</a></p>
-
+                                    <p><?php echo $isi?></p>
+                                    <a href="<?php echo base_url("$post_new->blogs_judul_seo") ?>" class="text-capitalize">Selengkapnya</a>
                                 </div>
                             </div>
                         </div>
                     </div>
-										<?php } ?>
-
+                   
+                   <?php } ?>
+                 
                 </div>
             </div>
         </div>
