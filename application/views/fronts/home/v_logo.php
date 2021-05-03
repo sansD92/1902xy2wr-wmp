5718d00c027b3730a495dfd5153c44c2caa498d3
arkans <82564511+arkansys22@users.noreply.github.com>
2021-05-03 16:30:37 +0700

    up

--- application/views/fronts/home/v_logo.php
+++ application/views/fronts/home/v_logo.php
@@ -4,29 +4,47 @@
         	<div class="col-md-12 animation" data-animation="fadeInUp" data-animation-delay="0.2s">
             	<div class="cl_logo_slider owl-carousel owl-theme" data-margin="30" data-loop="true" data-autoplay="true" data-dots="false" data-autoplay-timeout="2000">
                 	<div class="item">
-                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/pstnew.jpeg" alt="cl_logo1"/></a>
+                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/1.jpg" /></a>
                     </div>
                     <div class="item">
-                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/camnew.jpeg" alt="cam"/></a>
+                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/2.jpg" /></a>
                     </div>
                     <div class="item">
-                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/pwmnew.jpeg" alt="cam"/></a>
+                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/3.jpg" /></a>
                     </div>
                     <div class="item">
-                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/garutnew.jpeg" alt="cam"/></a>
+                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/3.jpg" /></a>
                     </div>
                     <div class="item">
-                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/wmunew.jpeg" alt="cam"/></a>
+                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/4.jpg" /></a>
                     </div>
                     <div class="item">
-                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/wmsnew.jpeg" alt="cam"/></a>
+                    	<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/5.jpg"/></a>
                     </div>
                       <div class="item">
-                        <a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/lmpnew.jpeg" alt="cam"/></a>
+                        <a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/6.jpg" /></a>
                     </div>
                       <div class="item">
-                        <a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/keinew.jpeg" alt="cam"/></a>
+                        <a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/7.jpg" /></a>
                     </div>
+										<div class="item">
+											<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/8.jpg" /></a>
+									</div>
+									<div class="item">
+										<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/9.jpg" /></a>
+								</div>
+								<div class="item">
+									<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/10.jpg" /></a>
+							</div>
+							<div class="item">
+								<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/11.jpg" /></a>
+						</div>
+						<div class="item">
+							<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/12.jpg" /></a>
+					</div>
+					<div class="item">
+						<a href="#"><img src="<?php echo base_url()?>assets/frontend/perusahaangrub/13.jpg" /></a>
+				</div>
                 </div>
             </div>
         </div>
