121b8c27c22b586938b7980a4f61c22c2000b5db
sansD92 <adjiedwisandy@gmail.com>
2021-05-03 10:02:27 +0700

    UP

--- application/views/fronts/tentangkami/v_dewandireksi.php
+++ application/views/fronts/tentangkami/v_dewandireksi.php
@@ -40,7 +40,7 @@
         <div class="row align-items-center">
             <div class="col-md-12 col-sm-12 mb-12 mb-lg-0 ">
                     <div class="heading_s1 text-center">
-                    <h2>Dewan Direksi</h2>
+                    <h2>Dewan Direksi WMP</h2>
                 </div>
                 <div>
                     <img style="width: 100%;height: auto"src="<?php echo base_url()?>assets/frontend/direksi.jpg" alt="filosofi"/>
