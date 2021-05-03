1f09b7c00b81173b9e0aacf25ca25911972c7dcd
sansD92 <adjiedwisandy@gmail.com>
2021-05-03 10:25:24 +0700

    Update v_about.php

--- application/views/fronts/home/v_about.php
+++ application/views/fronts/home/v_about.php
@@ -4,7 +4,7 @@
             <div class="col-md-6" data-animation="fadeInLeft" data-animation-delay="0.2s">
                 <div class="overlay_bg about_video_img mb-4 mb-lg-0">
                     <img src="<?php echo base_url()?>assets/frontend/about23.jpeg" alt="about_img4"/>
-                    <a href="https://www.youtube.com/watch?v=7e90gBu4pas" class="video_popup video_play"><img class="bounce_img" src="<?php echo base_url()?>assets/images/play-icon.png" alt="play-icon" allow="autoplay; encrypted-media"></a>
+                    <a link="true" href="https://www.youtube.com/watch?v=jPSzKyY5NnY" class="video_popup video_play"><img class="bounce_img" src="<?php echo base_url()?>assets/images/play-icon.png" alt="play-icon" allow="autoplay"></a>
                 </div>
             </div>
             <div class="col-md-6 col-sm-12 animation" data-animation="fadeInRight" data-animation-delay="0.4s">
