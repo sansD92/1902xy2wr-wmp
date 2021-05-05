b37a824024ee739934e782579ef0c9c2a1abd31d
arkans <82564511+arkansys22@users.noreply.github.com>
2021-05-05 15:10:24 +0700

    up

--- application/views/fronts/home/v_counter.php
+++ application/views/fronts/home/v_counter.php
@@ -5,7 +5,7 @@
             	<div class="heading_s1 heading_light text-center text-md-left">
                 	<h2>Highlight</h2>
                 </div>
-                <p>Bergabunglah Bersama Widodo Makmur Perkasa</p>
+                <h3>Bergabunglah Bersama Widodo Makmur Perkasa</h3>
                 <a href="#" class="btn btn-outline-white">Gabung</a>
             </div>
         	<div class="col-lg-6 offset-lg-1 col-md-7 col-sm-12 col-11 animation" data-animation="fadeInUp" data-animation-delay="0.4s">
@@ -14,28 +14,28 @@
                         <div class="box_counter text-center">
                             <img src="<?php echo base_url()?>assets/frontend/campur/icon1.png">
                             <h3 class="counter_text"><span class="counter">5</span></h3>
-                            <p>Lini Bisnis</p>
+                            <h3>Lini Bisnis</h3>
                         </div>
                     </div>
                     <div class="col-6 couter_border">
                         <div class="box_counter text-center">
                             <img src="<?php echo base_url()?>assets/frontend/campur/icon3.png">
                             <h3 class="counter_text"><span class="counter">25</span></h3>
-                            <p>Tahun Beroperasi</p>
+                            <h3>Tahun Beroperasi</h3>
                         </div>
                     </div>
                     <div class="col-6 couter_border">
                         <div class="box_counter text-center">
                             <img src="<?php echo base_url()?>assets/frontend/campur/icon4.png">
                             <h3 class="counter_text"><span class="counter">1558</span>+</h3>
-                            <p>SDM di Wilayah Indonesia</p>
+                            <h3>SDM di Wilayah Indonesia</h3>
                         </div>
                     </div>
                     <div class="col-6 couter_border">
                         <div class="box_counter text-center">
                             <img src="<?php echo base_url()?>assets/frontend/campur/icon5.png">
                             <h3 class="counter_text"><span class="counter">505</span></h3>
-                            <p>Item Produk</p>
+                            <h3>Item Produk</h3>
                         </div>
                     </div>
                 </div>
