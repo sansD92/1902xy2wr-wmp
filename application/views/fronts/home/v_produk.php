f5a197b1bf07ba6a9ced1c4bb0c9295075f91823
sansD92 <adjiedwisandy@gmail.com>
2021-06-07 14:10:57 +0700

    up

---
+++ application/views/fronts/home/v_produk.php
@@ -0,0 +1,149 @@
+<section class="small_pb">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12" >
+                <div class="heading_s4 text-center">
+                    <span class="sub_title">Produk Terbaik</span>
+                    <h2>Produk Kami</h2>
+                </div>
+                <p><center>Dengan bangga memperkenalkan produk-produk kami</center></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="cleafix small_divider"></div>
+            </div>
+        </div>
+        <div class="row mb-3 mb-sm-5 animation" data-animation="fadeInUp" data-animation-delay="0.4s">
+            <div class="col-md-12 text-center">
+                <ul class="list_none portfolio_filter filter_tab4">
+                    <li><a href="#" class="current" data-filter="*">all</a></li>
+                    <li><a href="#"  data-filter=".livestock">Sapi</a></li>
+                    <li><a href="#" data-filter=".meat">Daging Sapi</a></li>
+                    <li><a href="#" data-filter=".meatp">Produk Olahan</a></li>
+                    <li><a href="#" data-filter=".leather">Kulit</a></li>
+                    <li><a href="#" data-filter=".poultry">Produk Ayam</a></li>
+                    <li><a href="#" data-filter=".comodity">Komoditas</a></li>
+                    <li><a href="#" data-filter=".konstruksi">jasa konstruksi dan energi </a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <ul class="portfolio_container gutter_small work_col3 portfolio_gallery portfolio_style5 animation" data-animation="fadeInUp" data-animation-delay="0.6s">
+                    <li class="grid-sizer"></li>
+                    <!-- START PORTFOLIO ITEM -->
+                    <li class="portfolio-item livestock">
+                        <div class="portfolio_item">
+                            <a href="#" class="image_link">
+                                <img src="<?php echo base_url()?>assets/frontend/produk/350.jpg" alt="image">
+                            </a>
+                                <div class="portfolio_content">
+                                    <div class="link_container">
+                                        
+                                        <a href="<?php echo base_url()?>produk1"><i class="ion-plus"></i></a>
+                                    </div>
+                                    <h5><a href="<?php echo base_url()?>produk1">SAPI</a></h5>
+                                    <p>Kalimat Produk Sapi.</p>
+                              </div>
+                        </div>
+                    </li>
+                    <!-- END PORTFOLIO ITEM -->
+                   
+
+                     <!-- START PORTFOLIO ITEM -->
+                    <li class="portfolio-item meat">
+                        <div class="portfolio_item">
+                            <a href="#" class="image_link">
+                                <img src="<?php echo base_url()?>assets/frontend/produk/meat1.jpg" alt="image">
+                            </a>
+                                <div class="portfolio_content">
+                                    <div class="link_container">
+                                        
+                                        <a href="<?php echo base_url()?>produk2"><i class="ion-plus"></i></a>
+                                    </div>
+                                    <h5><a href="<?php echo base_url()?>produk2">DAGING SAPI</a></h5>
+                                    <p>Kalimat Daging Sapi</p>
+                              </div>
+                        </div>
+                    </li>
+                    <!-- END PORTFOLIO ITEM -->
+
+                     <!-- START PORTFOLIO ITEM -->
+                    <li class="portfolio-item meatp">
+                        <div class="portfolio_item">
+                            <a href="#" class="image_link">
+                                <img src="<?php echo base_url()?>assets/frontend/produk/meatpros1.jpg" alt="image">
+                            </a>
+                                <div class="portfolio_content">
+                                    <div class="link_container">
+                                        
+                                        <a href="<?php echo base_url()?>produk3"><i class="ion-plus"></i></a>
+                                    </div>
+                                    <h5><a href="<?php echo base_url()?>produk3">PRODUK OLAHAN</a></h5>
+                                    <p>Kalimat PRODUK OLAHAN</p>
+                              </div>
+                        </div>
+                    </li>
+                    <!-- END PORTFOLIO ITEM -->
+
+                      <!-- START PORTFOLIO ITEM -->
+                    <li class="portfolio-item leather">
+                        <div class="portfolio_item">
+                            <a href="#" class="image_link">
+                                <img src="<?php echo base_url()?>assets/frontend/produk/leather1.jpg" alt="image">
+                            </a>
+                                <div class="portfolio_content">
+                                    <div class="link_container">
+                                        
+                                        <a href="<?php echo base_url()?>produk4"><i class="ion-plus"></i></a>
+                                    </div>
+                                    <h5><a href="<?php echo base_url()?>produk4">LEATHER</a></h5>
+                                    <p>Kalimat Leather</p>
+                              </div>
+                        </div>
+                    </li>
+                    <!-- END PORTFOLIO ITEM -->
+
+                      <!-- START PORTFOLIO ITEM -->
+                    <li class="portfolio-item poultry">
+                        <div class="portfolio_item">
+                            <a href="#" class="image_link">
+                                <img src="<?php echo base_url()?>assets/frontend/produk/poultry1.jpg" alt="image">
+                            </a>
+                                <div class="portfolio_content">
+                                    <div class="link_container">
+                                        
+                                        <a href="<?php echo base_url()?>produk5"><i class="ion-plus"></i></a>
+                                    </div>
+                                    <h5><a href="<?php echo base_url()?>produk5">POULTRY</a></h5>
+                                    <p>Kalimat Poultry</p>
+                              </div>
+                        </div>
+                    </li>
+                    <!-- END PORTFOLIO ITEM -->
+
+                     <!-- START PORTFOLIO ITEM -->
+                    <li class="portfolio-item comodity">
+                        <div class="portfolio_item">
+                            <a href="#" class="image_link">
+                                <img src="<?php echo base_url()?>assets/frontend/produk/comodity1.jpg" alt="image">
+                            </a>
+                                <div class="portfolio_content">
+                                    <div class="link_container">
+                                        
+                                        <a href="<?php echo base_url()?>produk6"><i class="ion-plus"></i></a>
+                                    </div>
+                                    <h5><a href="<?php echo base_url()?>produk6">KOMODITAS</a></h5>
+                                    <p>Kalimat Komoditas</p>
+                              </div>
+                        </div>
+                    </li>
+                    <!-- END PORTFOLIO ITEM -->
+
+                   
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
--- application/views/fronts/produk/banner.php
+++ application/views/fronts/produk/banner.php
@@ -2,19 +2,7 @@
 <section class="background_bg breadcrumb_section overlay_bg2 page-title-light" style="height:500px;"data-img-src="<?php echo base_url()?>assets/frontend/campur/headersubmenu.jpg">
 	<div class="container">
     	<div class="row">
-        	<div class="col-md-12 text-center">
-            	<div class="page-title">
-            		<h1>Page Title Background</h1>
-                </div>
-                <nav aria-label="breadcrumb">
-                  <ol class="breadcrumb justify-content-center">
-                    <li class="breadcrumb-item"><a href="#">Beranda</a></li>
-                    <li class="breadcrumb-item"><a href="#">Produk</a></li>
-                    <li class="breadcrumb-item active" aria-current="page">Page Title Background</li>
-                  </ol>
-                </nav>
-            </div>
-        </div>
+        	
     </div>
 </section>
 <!-- END SECTION BANNER -->
