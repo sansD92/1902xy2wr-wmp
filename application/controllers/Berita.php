2d990ad00c84470d435cc063daedcf337fe60653
arkans <82564511+arkansys22@users.noreply.github.com>
2021-06-09 10:26:07 +0700

    up

--- application/controllers/Berita.php
+++ application/controllers/Berita.php
@@ -49,7 +49,7 @@ class Berita extends CI_Controller {
 		$data['posts_bisnis'] = $this->Crud_m->view_where_orders('bisnis','bisnis_status','bisnis_id','ASC');
 		 $data['status']   = 'active';
 		  $data['status_produk']   = '';
-		
+		$data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
 		$data['post_terbaru']= $this->Crud_m->view_one_limit('blogs','blogs_status','blogs_id','desc',$dari,$config['per_page']);
 
 		$this->pagination->initialize($config);
