b34e7a16296dac05228f7f362435247cb494c99a
sansD92 <adjiedwisandy@gmail.com>
2021-07-28 13:26:10 +0700

    up

--- application/views/fronts/tentangkami/v_strukturorganisasi.php
+++ application/views/fronts/tentangkami/v_strukturorganisasi.php
@@ -44,7 +44,7 @@
                     <h2>Struktur Organisasi</h2>
                 </div>
             	<div>
-            		<img style="width: 100%;height: auto"src="<?php echo base_url()?>assets/frontend/SO1.png" alt="struktur_organisasi"/>
+            		<img style="width: 100%;height: auto"src="<?php echo base_url()?>assets/frontend/stowmp.png" alt="struktur_organisasi"/>
                 </div>
             </div>
 
