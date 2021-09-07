74131edabd1b432f508d0b3e5b33246f911a2869
sansD92 <adjiedwisandy@gmail.com>
2021-09-07 16:17:10 +0700

    up

---
+++ application/controllers/Esg.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Esg extends CI_Controller {
+
+    function __construct()
+  {
+    parent::__construct();
+    /* memanggil model untuk ditampilkan pada masing2 modul */
+    /* memanggil function dari masing2 model yang akan digunakan */
+    $this->load->model(array('Crud_m'));
+  }
+
+   public function kei()
+{
+
+      $data['status']   = 'active';
+      $data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
+      $data['posts_bisnis'] = $this->Crud_m->view_where_orders('bisnis_kategori','bisnis_kategori_status','bisnis_kategori_id','ASC');
+      $data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
+
+
+
+  $this->load->view('fronts/esg/v_kei',$data);
+}
+}
---
+++ application/views/fronts/esg/v_kei.php
@@ -0,0 +1,169 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- START SECTION HEAD -->
+<?php $this->load->view('fronts/head.php')?>
+<!-- END SECTION HEAD -->
+
+<body>
+
+
+
+<!-- START HEADER -->
+<?php $this->load->view('fronts/header.php')?>
+<!-- END HEADER -->
+
+<!-- START SECTION BANNER -->
+<section class="background_bg breadcrumb_section overlay_bg2 page-title-light" data-img-src="<?php echo base_url()?>assets/frontend/campur/Subtentangkami.png">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <div class="page-title">
+                    <h1>Yayasan KEI</h1>
+                </div>
+                <nav aria-label="breadcrumb">
+                  <ol class="breadcrumb justify-content-center">
+                    <li class="breadcrumb-item"><a href="#">Beranda</a></li>
+                    <li class="breadcrumb-item"><a>Tentang Kami</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">Profil Perusahaan</li>
+                  </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END SECTION BANNER -->
+
+<!-- START SECTION ABOUT US -->
+<section class="small_pb overflow_hide">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-12 col-sm-12 mb-12 mb-lg-0 ">
+                <div class="col-md-12 col-sm-12 mb-12 mb-lg-0 ">
+                    <div class="heading_s1 text-center">
+                    <h2>KESATRIAAN ENTREPRENEUR INDONESIA (KEI)</h2>
+                </div>
+              <div class="row">
+            <div class="col-md-12 mb-2 mb-md-4 animation" >    
+                <h5>Profil Kesatriaan Entrepreneur Indonesia</h5>
+                <p style="text-align: justify;">Kesatriaan Entrepreneur Indonesia merupakan sebuah badan yang berada di bawah naungan PT. Widodo Makmur Perkasa. Berdiri dengan akta notaris ……dan diresmikan berdirinya pada ……. Berpusat di Joglo Tumiyono, desa Ngerangan, Kecamatan Bayat, Kabupaten Klaten,  Ksatriaan Entrepreneur Indonesia bertekad untuk menciptakan 500.000 entrepreneur muda.</p>
+                <P style="text-align: justify;">Saat ini dari jumlah total penduduk usia produktif Indonesia memiliki kurang lebih 62 juta jiwa dimana 31,4 juta jiwa bekerja di sector formal. Target pemerintah saat ini menciptakan setidaknya 4 juta jiwa entrepreneur muda sementara saat ini jumlah entrepreneur di Indonesia diperkirakan 2,885 juta jiwa. Pemerintah masih membutuhkan sekitar 1,115 juta entrepreneur yang diusahakan 900,000 darinya berusia muda, </P>
+                <P style="text-align: justify;">Karenanya Kesatriaan Entrepreneur Indonesia berusaha mendorong terciptanya para entrepreneur muda itu melalui pelatihan, training, inkubasi bisnis, kegiatan role model, dan melibatkan pihak ke tiga untuk membantu terlaksananya target penciptaan 500 ribu entrepreneur muda baru yang punya kapasitas dan kompetitif dengan mendeskripsikan visi misi KEI sebagai berikut;</P>
+                <h5>Visi</h5>
+                <p style="text-align: justify;">1. Menggerakkan Ekonomi Nasional dengan menciptakan entrepreneur muda dan lahirnya usaha baru.</p>
+                <h5>Misi</h5>
+                <p style="text-align: justify;">1. Melahirkan entrepreneur muda melalui pengembangan ketrampilan, pengetahuan serta mindset untuk memulai, membangun dan memimpin usaha yang mandiri.</p>
+                 <p style="text-align: justify;">2. Memperkuat pelaku entrepreneur agar mampu menciptakan lebih banyak lapangan kerja baru.</p>
+            </div>
+        </div>
+            </div>
+
+ <div class="col-md-12 mb-2 mb-md-4 animation">    
+                <h5>Kegiatan di Ksatriaan Entrepreneur Indonesia (KEI) meliputi :</h5>
+
+                <h6>1. Pendidikan dan Pelatihan</h6>
+                <p style="text-align: justify;">Joglo Tumiyono menyelenggarakan pendidikan entrepreneur di bidang peternakan (sapi dan unggas) serta pertanian bagi generasi muda yang potensial, berpendidikan, cerdas, berkomitmen terhadap bidang usaha yang digelutinya, memiliki kemauan kuat untuk belajar dan berusaha agar mampu menciptakan usaha-usaha baru yang ditangani secara cerdas.Juga berkomitmen terhadap pendidikan dan pengembangan Sumber Daya Manusia, Joglo Tumiyono juga menyelenggarakan berbagai program pendidikan dan pelatihan untuk menambah keahlian SDM, terutama dari kalangan internal WMP Group dan masyarakat sekitar. Hiasan relief ukiran yang menghiasi hampir seluruh bangunan Joglo Tumiyono dan berisi berbagai peristiwa bersejarah dari masa kerajaan, pra kemerdekaan, masa perjuangan mempertahankan kemerdekaan hingga kondisi terkini dengan dihadirkannya tokoh-tokoh presiden RI dari masa ke masa. Berharap generasi muda belajar nilai-nilai kehidupan dari keteladanan para tokoh bangsa, para pahlawan, founding fathers dan raja-raja penguasa masa lalu yang mampu menyatukan wilayah nusantara dengan keyakinan teguh yang dipegangnya. 
+</p>
+
+
+
+<h6>2. Membangun Kemitraan</h6>
+                <p style="text-align: justify;">Membangun kemitraan yang saling menguntungkan, untuk meningkatkan kesejahteraan petani, peternak dan UMKM. Ada kurang lebih 4 juta usia muda yang potensial dididik untuk menjadi petani, peternak dan pelaku UMKM yang akan mengolah produknya dengan system pertanian yang telah disempurnakan metodenya agar menghasilkan panen yang maksimal dan mengurangi potensi kegagalan panen atau kerugian. Sementara UMKM akan dibekali dengan kelahlian mengelola keuangan, asetnya dan menjaga cashflow serta metode pemasaran yang efektif.
+Ke depan pengembangan kemitraan akan berjalan berdampingan dengan pertumbuhan perusahaan. Saat ini kegiatan kemitraan yang sudah berjalan didukung oleh dua anak perusahaan WMP yaitu PT. Pasir Tengah (PST) yang melaksankan Kemitraan Sapi dan PT. Widodo Makmur Unggas (WMU) yang melaksanakan kemitraan ayam. </p>
+<h6>Kemitraan Sapi</h6>
+                <p style="text-align: justify;">Kemitraan sapi potong PT Pasir Tengah dilaksanakan untuk membantu para peternak yang sering mengalami permasalahan dalam usahanya. Kendala yang sering dialami oleh para peternak pada saat melakukan usaha pemeliharaan sapi potong diantaranya :  kesulitan akses modal di lembaga keuangan (dikenakan bunga yang tinggi), tidak adanya komitmen,  transfer teknologi, resiko kegagalan perkawinan, resiko kematian, harga panen yang bergantung pada tengkulak, sapi yang tidak dibayarkan saat transaksi, dll.
+. 
+</p>
+<p style="text-align: justify;">Kemitraan sapi potong PT Pasir Tengah dibentuk sebagai salah satu bentuk program Corporate Social Responsibility (CSR) yang bertujuan meningkatkan pendapatan dan  kesejahteraan peternak lokal. PT Pasir Tengah bekerjasama dengan beberapa stakeholder membantu mensukseskan program Kemitraan sapi potong dengan melibatkan :</p>. 
+
+<p>1) Kelembagaan Peternak</p>
+
+<p>2) Lembaga Pembiayaan/Perbankan</p>
+
+<p>3) Perusahaan Asuransi</p>
+
+<p>4) Pemerintah Daerah</p>
+
+<p style="text-align: justify;">Peran serta beberapa stakeholder diharapkan mampu memberikan keuntungan dan kemudahan bagi peternak yang mengikuti program kemitraan tersebut.
+
+Adapun tujuan Kemitraan adalah :</p>
+
+<p>1) Peternak dibantu memperoleh akses permodalan di lembaga keuangan (berbunga rendah)</p>
+
+<p>2) Peternak mendapatkan pendampingan dan transfer teknologi langsung dari tim  peternakan internal PT Pasir Tengah</p>
+
+<p>3) Peternak memperoleh jaminan pakan kosentrat yang berkualitas tinggi</p>
+
+<p>4) Peternak memperoleh perlindungan asuransi terhadap sapi yang dipelihara hingga 100% dari total nilai sapi</p>
+<p>5) Peternak memperoleh jaminan pasar atas sapi potongnya</p>
+<p>6) Peternak memperoleh harga sapi yang kompetitif dan bersaing di pasar</p>
+
+<p style="text-align: justify;">Program ini pertama kali diresmikan pada tanggal 9 Juli 2017 di Wonogiri, Jawa Tengah dengan Mitra dari Kelembagaan Peternak yaitu Badan Usaha Milik Petani (BUMP) PT Pengayom Tani Sejagad dengan total sapi Mitra sejumlah 115 ekor. Pengembangan tahun 2018 dilakukan di daerah Cianjur, Garut, Boyolali dengan Koperasi Serba Usaha (KSU)  dan Asosiasi Sapi Potong Indonesia (ASPIN).
+
+Tahun 2019 PT Pasir Tengah melakukan pengembangan di Karanganyar dan Daerah Istimewa Yogyakarta. Jumlah sapi yang didistribusikan dalam 3 tahun ini mencapai 1.105 ekor.
+
+Beberapa lembaga keuangan negeri dan swasta telah berkomitmen bekerja sama sebagai penyedia modal kerja dan asuransi. Hingga saat ini lembaga pembiayaan telah berhasil memberikan fasilitas kredit kepada peternak binaan PT Pasir Tengah  sebesar 16,8 Milyar dalam kurun waktu 3 tahun dengan skema Kredit Usaha Rakyat (KUR) maupun komersial.
+
+Asuransi ternak sapi menjadi salah satu elemen penting dalam mencapai keberhasilan usaha ternak sapi di peternakan rakyat, karena mampu memberi jaminan terhadap aset (sapi) yang dimiliki oleh peternak. Beberapa perusahaan asuransi telah menjadi mitra kerja kami.
+
+Kemitraan ini menggunakan sistem transparasi data yang baik sehingga meningkatkan rasa saling percaya, dilengkapi analisa lapangan yang akurat oleh PT Pasir Tengah serta didukung oleh perbankan yang akan mejaga dan mengatur lalu lintas keuangan peternak sehingga arus kas berjalan lancar. Oleh karena itu kemitraan ini diharapkan mampu direplikasi didaerah lain sehingga menjadi salah satu contoh permodelan kemitraan yang baik serta aman dalam rangka menambah pendapatan dan menambah daya saing peternak demi keberlanjutan dunia usaha peternakan rakyat.</p>
+
+
+<h6>Kemitraan Ayam</h6>
+                <p style="text-align: justify;">Saat ini Program Kemitraan ayam sedang digodog kembali oleh manajemen perusahaan, Divisi Research and Development WMP, serta managemen KEI. Saat konsep telah matang dan siap dilaksankan akan kami sampaikan pada website ini. 
+</p>
+
+<h6>3. Inkubator Bisnis</h6>
+                <p style="text-align: justify;">Ksatriaan Entrepreneur Indonesia juga memberikan pelayanan konsultasi dan pendampingan bisnis bagi entrepreneur muda yang memulai membangun usaha salah satunya dengan menghubungkan pada lembaga bank atau lembaga keuangan lainnya untuk mendapatkan fasilitas pembiayaan. Juga pihak-pihak terkait lainnya seperti supplier atau lembaga  swadaya lainnya untuk mengadakan kerjasama usaha. </p>
+                
+            </div>
+        </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END SECTION ABOUT US -->
+
+
+
+<!-- START SECTION SUBCRIBE -->
+<?php $this->load->view('fronts/home/v_subcribe')?>
+<!-- END SECTION SUBCRIBE -->
+
+<!-- START FOOTER SECTION -->
+<?php $this->load->view('fronts/footer')?>
+<!-- END FOOTER SECTION -->
+
+<a href="#" class="scrollup" style="display: none;"><i class="ion-ios-arrow-up"></i></a>
+
+<!-- Latest jQuery -->
+<script src="<?php echo base_url()?>assets/js/jquery-1.12.4.min.js"></script>
+<!-- jquery-ui -->
+<script src="<?php echo base_url()?>assets/js/jquery-ui.js"></script>
+<!-- popper min js -->
+<script src="<?php echo base_url()?>assets/js/popper.min.js"></script>
+<!-- Latest compiled and minified Bootstrap -->
+<script src="<?php echo base_url()?>assets/bootstrap/js/bootstrap.min.js"></script>
+<!-- owl-carousel min js  -->
+<script src="<?php echo base_url()?>assets/owlcarousel/js/owl.carousel.min.js"></script>
+<!-- magnific-popup min js  -->
+<script src="<?php echo base_url()?>assets/js/magnific-popup.min.js"></script>
+<!-- waypoints min js  -->
+<script src="<?php echo base_url()?>assets/js/waypoints.min.js"></script>
+<!-- parallax js  -->
+<script src="<?php echo base_url()?>assets/js/parallax.js"></script>
+<!-- countdown js  -->
+<script src="<?php echo base_url()?>assets/js/jquery.countdown.min.js"></script>
+<!-- fit video  -->
+<script src="<?php echo base_url()?>assets/js/jquery.fitvids.js"></script>
+<!-- jquery.counterup.min js -->
+<script src="<?php echo base_url()?>assets/js/jquery.counterup.min.js"></script>
+<!-- isotope min js -->
+<script src="<?php echo base_url()?>assets/js/isotope.min.js"></script>
+<!-- elevatezoom js -->
+<script src='<?php echo base_url()?>assets/js/jquery.elevatezoom.js'></script>
+<!-- scripts js -->
+<script src="<?php echo base_url()?>assets/js/scripts.js"></script>
+
+</body>
+</html>
