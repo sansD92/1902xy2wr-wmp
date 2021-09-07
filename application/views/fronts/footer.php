801a4a1db86dfe4b0970de162818205dde11206d
sansD92 <adjiedwisandy@gmail.com>
2021-09-07 15:06:42 +0700

    up

--- application/views/fronts/footer.php
+++ application/views/fronts/footer.php
@@ -4,7 +4,7 @@
             <div class="row">
                 <div class="col-lg-6 col-md-12" >
                     <h2>BERHUBUNGAN DENGAN KAMI</h2>
-                    <p>Menjadi Perusahaan Pangan dan Papan Terbesar di Asia Tenggara.</p>
+                    <p>Sebagai Perusahaan Penyedia Pangan Bangsa.</p>
                     <ul class="list_none footer_social">
 
                         <li><a href="<?php echo $identitas->youtube?>"><i class="ion-social-youtube-outline" ></i></a></li>
