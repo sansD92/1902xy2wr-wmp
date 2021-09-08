d3412f55d72407032aa5a7842ae3224944c18151
sansD92 <adjiedwisandy@gmail.com>
2021-09-08 15:59:24 +0700

    up

--- application/views/fronts/esg/v_joglo.php
+++ application/views/fronts/esg/v_joglo.php
@@ -14,7 +14,7 @@
 <!-- END HEADER -->
 
 <!-- START SECTION BANNER -->
-<section class="background_bg breadcrumb_section overlay_bg2 page-title-light" data-img-src="<?php echo base_url()?>assets/frontend/campur/Subtentangkami.png">
+<section style="height:500px;" class="background_bg breadcrumb_section overlay_bg2 page-title-light" data-img-src="<?php echo base_url()?>assets/frontend/campur/Sub-Joglo2.png">
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
