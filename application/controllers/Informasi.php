b7f1645bec6b540ba502f8c013d7e29e23549e47
arkans <82564511+arkansys22@users.noreply.github.com>
2021-09-08 17:03:11 +0700

    up

--- application/controllers/Informasi.php
+++ application/controllers/Informasi.php
@@ -9,16 +9,19 @@ class Informasi extends CI_Controller {
     /* memanggil function dari masing2 model yang akan digunakan */
     $this->load->model(array('Crud_m'));
   }
+
   public function faq()
-{
+  {
       $data['status']   = 'active';
       $data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
       $data['posts_bisnis'] = $this->Crud_m->view_where_orders('bisnis_kategori','bisnis_kategori_status','bisnis_kategori_id','ASC');
+    $this->load->view('fronts/informasi/v_faq',$data);
+  }
+  public function gallery()
+  {
+      $data['status']   = 'active';
       $data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
-
-
-
-
-  $this->load->view('fronts/informasi/v_faq',$data);
-}
+      $data['posts_bisnis'] = $this->Crud_m->view_where_orders('bisnis_kategori','bisnis_kategori_status','bisnis_kategori_id','ASC');
+    $this->load->view('fronts/informasi/v_gallery',$data);
+  }
 }
