e828299f4e2ffabe230d21aca6775f2c9666c334
sansD92 <adjiedwisandy@gmail.com>
2021-09-08 14:40:44 +0700

    up

--- application/controllers/Informasi.php
+++ application/controllers/Informasi.php
@@ -11,10 +11,11 @@ class Informasi extends CI_Controller {
   }
   public function faq()
 {
+      $data['status']   = 'active';
+      $data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
+      $data['posts_bisnis'] = $this->Crud_m->view_where_orders('bisnis_kategori','bisnis_kategori_status','bisnis_kategori_id','ASC');
+      $data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
 
-			$data['status']   = 'active';
-
-			$data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
 
 
 
