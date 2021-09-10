3c9d1df231f760f26d09d14c8feaf332b3e1edfa
sansD92 <adjiedwisandy@gmail.com>
2021-09-10 19:13:43 +0700

    up

--- application/controllers/Tatakelola.php
+++ application/controllers/Tatakelola.php
@@ -59,6 +59,18 @@ public function piagam_corporate()
 
   $this->load->view('fronts/tatakelola/new/v_corporate',$data);
 }
+public function piagam_audit()
+{
+      $data['status']   = 'active';
+      $data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
+      $data['posts_bisnis'] = $this->Crud_m->view_where_orders('bisnis_kategori','bisnis_kategori_status','bisnis_kategori_id','ASC');
+      $data['identitas']= $this->Crud_m->get_by_id_identitas($id='1');
+
+
+
+
+  $this->load->view('fronts/tatakelola/new/v_audit',$data);
+}
  public function penerapan_gcg()
 {
       $data['status']   = 'active';
--- application/views/fronts/header.php
+++ application/views/fronts/header.php
@@ -196,6 +196,7 @@
                                             <li><a class="dropdown-item nav-link nav_item" href="<?php echo base_url()?>tatakelola/piagam_komisaris" >Piagam Dewan Komisaris</a></li>
                                              <li><a class="dropdown-item nav-link nav_item" href="<?php echo base_url()?>tatakelola/pedoman_komite" >Pedoman Komite Nominasi & Remunerasi</a></li>
                                               <li><a class="dropdown-item nav-link nav_item" href="<?php echo base_url()?>tatakelola/piagam_corporate" >Piagam Corporate Secretary</a></li>
+                                              <li><a class="dropdown-item nav-link nav_item" href="<?php echo base_url()?>tatakelola/piagam_audit" >Piagam Internal Audit</a></li>
                                            
 
                                         </ul>
