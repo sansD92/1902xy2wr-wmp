02bcd394d14842bad28eab4930edae45d77d9a98
arkans <82564511+arkansys22@users.noreply.github.com>
2021-09-10 11:44:31 +0700

    up

--- application/views/fronts/esg/v_joglo.php
+++ application/views/fronts/esg/v_joglo.php
@@ -48,43 +48,24 @@
                 <h5>Profil Joglo Tumiyono</h5>
 
                 <p style="text-align: justify;">
-                  Joglo Tumiyono adalah sebuah mahakarya yang dipersembahkan PT Widodo Makmur Perkasa sebagai bagian dari proses pelestarian  budaya adiluhung Jawa khususnya terkait dengan seni bangunan dan pahat. Didedikasikan bagi generasi muda agar memiliki wawasan budaya dan menumbuhkan rasa memiliki terhadap budaya bangsa.
+                  Joglo Tumiyono adalah sebuah mahakarya yang dipersembahkan PT Widodo Makmur Perkasa, Tbk sebagai bagian dari proses pelestarian  budaya adiluhung Jawa khususnya terkait dengan seni bangunan dan pahat. Didedikasikan bagi generasi muda agar memiliki wawasan budaya dan menumbuhkan rasa memiliki terhadap budaya bangsa.
                 </p>
                 <p style="text-align: justify;">
-                  Kerajaan dan Kesultanan di wilayah Jawa berdasarkan catatan sejarah, adalah kerajaan besar yang memiliki wilayah yang luas dan Berjaya diantara kerajaan lainnya di sekitar Asia. Tetapi beberapa keraton besar tersebut musnah, tidak diketahui keberadaannya. Tidak meninggalkan artefak dan catatan sejarah. Keraton Demak, Keraton Majapahit, Keraton Singasari, Kesultanan Demak, Kesultanan Pajang semua hilang tanpa bekas sehingga kita tidak mengetahui kebesaran pada masa lalu.
-                </p>
-                <p style="text-align: justify;">
-                  Beruntungnya Kesultanan Yogyakarta dan Kasunanan Surakarta masih terjaga keberadaannya sehingga bisa kita nikmati saat ini dan menjadi referensi pembangunan Joglo Tumiyono. Joglo Tumiyono didirikan bukan hanya sebagai monument dan penambah wawasan, tetapi juga memiliki visi yang mulia  sebagai pusat kegiatan masyarakat.
-                </p>
-                <p style="text-align: justify;">
-                  Joglo Tumiyono didirikan atas prakarsa pendiri PT. Widodo Makmur Perkasa dan didukung oleh seluruh keluarga besar Tri Widodo untuk mendirikan sebuah bangunan joglo termegah di dunia sebagai monumen dan pelestari budaya Jawa yang sarat nilai-nilai kehidupan. Joglo Tumiyono didedikasikan bagi pengembangan masyarakat dan tempat pendidikan dan pelatihan generasi muda sebagai bentuk pengabdian pada bangsa. Menempati luas lahan 2,2 hektar di Desa Ngerangan, Kecamatan Bayat, kabupaten Klaten Propinsi Jawa Tengah. Berada diantara kawasan pegunungan, Gunung Merapi, Makam Sunan Bayat maupun lokasi petilasan jalur gerilya Pangeran Diponegoro. Juga berdekatan dengan kawasan wisata Umbul Ponggok.
-                </p>
-                <p style="text-align: justify;">
-                  Peletakan batu pertama pada bulan Oktober 2017 dengan ditancapkannya paku emas di puncak bangunan serta keempat soko guru, Joglo Tumiyono dibangun berdasarkan konsep kawasan Joglo terpadu yang dibuat oleh para arsitek muda cerdas di JOSO, konsultan arsitek dan merupakan anak perusahaan PT. Widodo Makmur Perkasa yang berkolaborasi dengan pihak-pihak terkait lainnya. Joglo Tumiyono dibangun dari sebuah mimpi menghadirkan sebuah mahakarya seni yang bermanfaat bagi anak negeri.
+                  Joglo Tumiyono didirikan sebagai monument dan pelestari budaya Jawa yang sarat nilai-nilai kehidupan. Joglo Tumiyono didedikasikan bagi pengembangan masyarakat dan tempat pendidikan dan pelatihan generasi muda sebagai bentuk pengabdian pada bangsa. Menempati luas lahan 2,2 hektar di Desa Ngerangan, Kecamatan Bayat, kabupaten Klaten Propinsi Jawa Tengah.
                 </p>
                 <p style="text-align: justify;">
                   Joglo Tumiyono akan menjadi pusat pelatihan dan pembelajaran para entrepreneur muda yang terdidik dan terampil agar siap menjadi pelaku ekonomi berdasarkan minat mereka di bidang peternakan, pertanian atau pengolahan bahan makanan. Para entrepreneur muda ini akan dididik agar mampu mengelola bisnisnya dengan baik serta sustainability, yang merupakan sebuah kondisi dimana usaha dapat terus bertahan dan berkembang seiring waktu.
                 </p>
                 <p style="text-align: justify;">
-                  Joglo Tumiyono aktif mendukung masyarakat dan menyediakan tempat untuk pengembangan kualitas sumber daya manusia, di antaranya melalui kegiatan;
+                  Joglo Tumiyono aktif mendukung masyarakat dan menyediakan tempat untuk pengembangan kualitas sumber daya manusia, di antaranya melalui kegiatan.
+
                 </p>
-                <h6>1.	Pengembangan Ekonomi, Seni dan Budaya</h6>
+                <h6>1. Pengembangan Ekonomi, Seni dan Budaya</h6>
+                <h6>2. Pesantren Entrepreneur</h6>
+
+                <h6>Gallery</h6>
                 <p style="text-align: justify;">
-                  Joglo Tumiyono selain menjadi kawah candradimuka untuk menggembleng calon-calon entrepreneur muda untuk menjadi pengusaha kelas menengah yang produktif dan berperan sebagai lokomotif ekonomi Indonesia, Joglo Tumiyono ke depannya juga menjadi sentra pengembangan kawasan ekonomi kreatif berbasis potensi ekonomi lokal seperti pembinaan pada pelaku usaha angkringan mengingat Klaten merupakan tempat perintisan usaha angkringan.  Lurik Klaten yang mulai dikenal dan diminati para turis maupun masyarakat luar Klaten, usaha kripik belut, usaha gerabah dan  keramik juga membutuhkan sentuhan bimbingan pengelolaan agar lebih berkembang ke depannya. Joglo Tumiyono menyediakan barisan bangunan komersial di sisi luar yang bisa menjadi show case dan galeri produk unggulan UMKM setempat.
-                </p>
-                <p style="text-align: justify;">
-                  Dalam pengembangan Seni dan Budaya, Joglo Tumiyono mendukung sepenuhnya setiap usaha pelestarian budaya. Di antaranya, memberikan wadah bagi PEPADI (Persatuan Pendalangan Indonesia berkiprah melaksanakan pentas wayang setiap malam Sabtu Pahing di Joglo Tumiyono baik. Joglo Tumiyono juga menjadi tempat  pelantikan pengurus PEPADI Klaten masa bakti 2019-2024 pada Jumat malam Sabtu Pahing 09 Agustus 2019. Acara tersebut dihadiri Founder WMP, Bupati Klaten Ibu Sri Mulyani, Nusron Wahid Anggota DPR periode 2019-2024 dan tokoh masyarakat lainnya. Acara tersebut menjadi penanda mutualisme antara Joglo Tumiyono dengan PEPADI dan menjadi komitmen bagi Joglo TUmiyono untuk menyelenggarkan pentas wayang kulit secara rutin setiap malam Sabtu Pahing.
-                </p>
-                <h6>2.	Pesantren Entrepreneur</h6>
-                <p style="text-align: justify;">
-                  Dalam kawasan Joglo Tumiyono juga didirikan sekolah merangkap sebagai pesantren yang pengelolaannya dilaksankan berdasarkan kerjasama antara institusi pendidikan ternama di Turki, Sulaimaniyah dengan Yayasan Ksatriaan Entrepreneurship Indonesia (KEI). Para santri yang lolos seleksi menempati asrama yang ada di Pondok Pesantren yang dibangun megah di sebelah selatan kawasan terpadu Joglo Tumiyono. Para pendidik dipilih dari Turki dan Indonesia yang telah lolos selksi dan merupakan para pendidik berkualitas , berpendidikan tinggi, menguasai ilmu agama dan ilmu pembelajaran serta berahlak mulia.
-                </p>
-                <p style="text-align: justify;">
-                  Tujuan pendidikan di pesantren ini adalah menciptakan generasi muda yang cerdas, berahlak mulia, dan memiliki skill berorganisasi dan berwirausaha sebagai entrepreneur.  Karena entrepreneur berperan besar dalam mendorong pembangunan ekonomi suatu bangsa. Saat ini jumlah UMKM di Indonesia baru mencapai 2% dari total penduduk Indonesia. Pada negara-negara maju jumlah entrepreneirnya berkisar 4%. Masih perlu kerja keras kita untuk menjadikan lebih banyak penduduk usia produktif sebagai entrepreneur yang handal sebagai lokomotif pertumbuhan ekonomi nasional. Para entrepreneur akan dididik mengelola bisnisnya secara professional, disiplin dalam pengelolaan dan mampu berinovasi untuk memenuhi kebutuhan pasar.
-                </p>
-                <h6>3.	Gallery</h6>
-                <p style="text-align: justify;">
-                  Kumpulan foto-foto berbagai kegiatan yang dilaksanakan di Joglo Tumiyono (Kesatriaan Entrepreneur Indonesia
+                  Kumpulan foto-foto berbagai kegiatan yang dilaksanakan di Joglo Tumiyono (Kesatriaan Entrepreneur Indonesia)
                 </p>
 
 
