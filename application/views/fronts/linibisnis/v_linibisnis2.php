22856ddb68e9fe9220e89533139fdf7585c56c5d
arkans <82564511+arkansys22@users.noreply.github.com>
2021-09-11 17:35:26 +0700

    up

--- application/views/fronts/linibisnis/v_linibisnis2.php
+++ application/views/fronts/linibisnis/v_linibisnis2.php
@@ -19,7 +19,7 @@
         <div class="row">
 
             <div class="col-md-12 text-center">
-              <br><br><br>
+              <br>
                 <div class="page-title">
                     <h1><?php echo "$title"; ?></h1>
                 </div>
