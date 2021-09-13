60344907f5047fb3a5ea62bf47f2ec86ff95b7ce
sansD92 <adjiedwisandy@gmail.com>
2021-09-13 18:56:16 +0700

    up

--- application/views/fronts/beritas/v_detail.php
+++ application/views/fronts/beritas/v_detail.php
@@ -14,7 +14,7 @@
 <!-- END HEADER -->
 
 <!-- START SECTION BANNER -->
-<section class="background_bg breadcrumb_section overlay_bg2 page-title-light" style="height:500px;"data-img-src="<?php echo base_url()?>assets/frontend/campur/headersubmenu.jpg">
+<!-- <section class="background_bg breadcrumb_section overlay_bg2 page-title-light" style="height:500px;"data-img-src="<?php echo base_url()?>assets/frontend/campur/headersubmenu.jpg"> -->
 	
 </section>
 <!-- END SECTION BANNER -->
