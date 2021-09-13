8ee06212c6515d92a57fb33f8b3c9cbecd7d1173
arkans <82564511+arkansys22@users.noreply.github.com>
2021-09-13 10:50:29 +0700

    up

--- application/views/fronts/home/v_linibisnis2.php
+++ application/views/fronts/home/v_linibisnis2.php
@@ -10,10 +10,41 @@
                 </div>
             </div>
         </div>
-        <div class="row " style="justify-content:center;">
+
+        <div class="row animation" style="justify-content:center;" data-animation="fadeInUp" data-animation-delay="0.4s">
           <?php  foreach ($posts_bisnis as $post_new){
             ?>
+            <div class="col-md-4 mt-3 mt-md-4">
+            	<div class="flip_box  text_white">
+      					<center>
+                    <div class="front" >
+                      <img style="height:400px; width:400px" src="<?php echo base_url("assets/frontend/campur/bg_box_linibisnis_home.jpg") ?>" />
+                      <div class="inner">
+                              <img style="width: 150px;"src="<?php echo base_url("assets/frontend/linibisnis/$post_new->bisnis_kategori_gambar_icon") ?>" alt="Icon"/>
+          							        <h5><?php echo $post_new->bisnis_kategori_judul?></h5>
+          						</div>
+                  	</div>
+                  </center>
 
+                <div class="back">
+                    <img style="height:400px; width:400px" src="<?php echo base_url("assets/frontend/campur/bg_box_linibisnis_home2.jpg") ?>" alt="Icon"/>
+                    <div class="inner">
+
+                      				<p></p>
+                              <a href="<?php echo base_url("linibisnis/kategori/$post_new->bisnis_kategori_judul_seo") ?>" class="btn btn-outline-white">Selengkapnya</a>
+        						</div>
+                </div>
+				      </div>
+            </div>
+
+            <?php } ?>
+
+        </div>
+
+
+        <!-- <div class="row " style="justify-content:center;">
+          <?php  foreach ($posts_bisnis as $post_new){
+            ?>
         	<div class="col-lg-4 col-sm-6 text-center">
             	<div class="icon_box icon_box_style_8 text_white">
                     <div class="box_icon mb-3">
@@ -26,10 +57,7 @@
                     </div>
                 </div>
             </div>
-
             	<?php } ?>
-
-
-        </div>
+        </div>-->
     </div>
 </section>
