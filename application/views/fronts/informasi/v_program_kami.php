9e01ca743cd1636b27673a8acc07f78045163d73
sansD92 <adjiedwisandy@gmail.com>
2021-09-13 17:46:48 +0700

    up

--- application/views/fronts/informasi/v_program_kami.php
+++ application/views/fronts/informasi/v_program_kami.php
@@ -56,7 +56,65 @@
 </section>
 <!-- END SECTION ABOUT US -->
 
+<section>
+  <div class="container">
+      <div class="row">
 
+          <div class="col-lg-12">
+              <div class="row">
+
+          <?php  foreach ($posts_programkami as $post_new){
+                                    $jdl = character_limiter($post_new->blogs_judul,30);
+                                        ?>
+                    <div class="col-lg-4 col-md-6 mb-md-4 mb-2 pb-2">
+                        <div class="blog_post blog_style1">
+                            <div class="blog_img">
+                                <a href="<?php echo base_url("$post_new->blogs_judul_seo") ?>">
+                                    <img src="<?php echo base_url();?>assets/frontend/blogs/<?php echo $post_new->blogs_gambar ?>">
+                                </a>
+                            </div>
+                            <div class="blog_content bg-white">
+                                <div class="blog_text">
+                                    <h6 class="blog_title"><a href="<?php echo base_url("$post_new->blogs_judul_seo") ?>"><?php echo $jdl ?></a></h6>
+                                    <ul class="list_none blog_meta">
+                                        <li><a href="<?php echo base_url("$post_new->blogs_judul_seo") ?>"><i class="ion-calendar"></i> <?php echo  tgl_indo($post_new->blogs_post_tanggal); ?></a></li>
+
+                                    </ul>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                          <?php } ?>
+
+
+                </div>
+                <div class="row">
+                    <div class="col-12 mt-4">
+                        <ul class="pagination justify-content-center">
+
+                            <li ><?php
+  echo $this->pagination->create_links();
+  ?></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 mt-lg-0 mt-4 pt-3 pt-lg-0">
+              <div class="sidebar">
+                   
+
+                  
+
+                  
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
 
 <!-- START SECTION SUBCRIBE -->
 <?php $this->load->view('fronts/home/v_subcribe')?>
